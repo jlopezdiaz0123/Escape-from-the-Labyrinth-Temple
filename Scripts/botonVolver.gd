a45e7d8540bba6e9d73f2e283ee15a167a3c442c
jlopezdiaz0123 <jlopezdiaz0123@uoc.edu>
2024-06-20 19:22:17 +0200

    -

--- Scripts/botonVolver.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-
-func _process(_delta):
-	pass
-func _on_boton_volver_pressed():
-	get_tree().change_scene_to_file("res://Pantallas/menuInicio.tscn")
--- Scripts/etiquetaTiempoCompletado.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-func _ready():
-	var label_tiempo_completado = $LabelTiempoCompletado
-	label_tiempo_completado.text = "Tiempo completado: " + str(ControlJuego.tiempo_completado)
